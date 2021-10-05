89697ac4980e6a6a395dd05f0db3952c7ef85aeb
Kalle Jillheden <kalle.jillheden@iver.se>
2021-10-05 21:10:26 +0200

    Updated coloring to cyan

--- encoder.go
+++ encoder.go
@@ -39,8 +39,8 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-var escapedColor = color.New(color.BgBlack, color.FgYellow)
-var unescapedColor = color.New(color.BgBlack, color.FgYellow)
+var escapedColor = color.New(color.BgBlack, color.FgCyan)
+var unescapedColor = color.New(color.BgBlack, color.FgCyan)
 
 const upperHex = "0123456789ABCDEF"
 
